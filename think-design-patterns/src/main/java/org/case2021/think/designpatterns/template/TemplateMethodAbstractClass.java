8ac8b23439f53f1b7a9e836c7ea3805ee3918ee9
zhangheng <zhangheng@cdqcp.com>
2021-05-27 17:04:06 +0800

    [+]Template method design patterns

--- think-design-patterns/src/main/java/org/case2021/think/designpatterns/template/TemplateMethodAbstractClass.java
+++ think-design-patterns/src/main/java/org/case2021/think/designpatterns/template/TemplateMethodAbstractClass.java
@@ -3,9 +3,9 @@ package org.case2021.think.designpatterns.template;
 
 /**
  * 模版方法设计模式
- * <p>templateMethod()方式实现最基础的业务，其次会调用methodA() method()B方法来实现扩展</p>
- * <p>继承本模版的所有实际模版都要实现自己的模版，此处只是定义不做具体实现，或者只是做基础的实现，按需调整</p>
- * <p>作用:1、复用 2、扩展</p>
+ * <p>templateMethod()方式实现最基础的业务，其次会调用methodA() method()B方法来实现扩展
+ * <p>继承本模版的所有实际模版都要实现自己的模版，此处只是定义不做具体实现，或者只是做基础的实现，按需调整
+ * <p>作用:1、复用 2、扩展
  *
  * @see java.io.InputStream
  * @see java.io.ByteArrayInputStream
